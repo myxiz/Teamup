9ca1fff4bfb72911f0b483fbe847c4fec07a97b7
SparklinZ <bolin.zhu123@gmail.com>
2019-12-11 02:57:39 -0500

    fuck

--- teamUp/landing_render.js
+++ teamUp/landing_render.js
@@ -595,7 +595,7 @@ async function handleEditOwnCard(event) {
     try {
         const result = await getUserData(localStorage.getItem("name"));
         console.log(result);
-        $('#userPage').prepend(renderOwnEditStudentCard(result));
+        $('#userPageBody').prepend(renderOwnEditStudentCard(result));
      } catch (error) {
          console.log(error);
          result = error;
